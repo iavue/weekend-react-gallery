09b5c4bb26b5f572c15bdd3068abaa1ae6244e27
Ia Vue <iavue00@gmail.com>
2023-03-24 18:39:30 -0500

    completed GET on client side and started on gallerylist component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  let [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('Response.data from client GET:', response.data);
+      galleryList(response.data);
+      console.log('galleryList:', galleryList);
+    }).catch((err) => {
+      console.log('Error on client GET:', err);
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,5 @@
+function GalleryList({gallerylist}) {
+    return (
+        
+    )
+}
