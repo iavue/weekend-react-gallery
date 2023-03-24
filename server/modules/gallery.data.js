26aee99d17de7a204c3e314f5fd53f62a25648d7
Ia Vue <iavue00@gmail.com>
2023-03-24 16:19:00 -0500

    Added photo descriptions and IDs to server module

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', title: 'Goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/abstract.jpg', title: 'Abstract Art', description: 'Photo of waves in abstract art.', likes: 0 },
+    { id: 3, path: 'images/cow.jpg', title: 'Cow', description: 'Painting of a cow in beautiful colors.', likes: 0 },
+    { id: 4, path: 'images/fish.jpg', title: 'Fish', description: 'Photo of a cool looking fish with blue scales.', likes: 0 },
+    { id: 5, path: 'images/flower.jpg', title: 'Flower', description: 'Photo of a pretty flower.', likes: 0 },
+    { id: 6, path: 'images/woman.jpg', title: 'Woman', description: 'Photo of a woman with eyes closed.', likes: 0 }
 ];
 
 module.exports = galleryItems;
